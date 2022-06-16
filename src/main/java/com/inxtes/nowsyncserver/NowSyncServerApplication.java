dc83714190cb82dac43b5f43146c1f2e35b011e7
ayaya <inxtes@gmail.com>
2022-06-16 17:05:39 +0800

    Initialize the frame

---
+++ src/main/java/com/inxtes/nowsyncserver/NowSyncServerApplication.java
@@ -0,0 +1,13 @@
+package com.inxtes.nowsyncserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowSyncServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NowSyncServerApplication.class, args);
+    }
+
+}
